423a990541b976257bd18ff1579599bf8207348a
PiPranav111 <104920398+PiPranav111@users.noreply.github.com>
2022-05-27 17:42:41 +0530

    Add files via upload

---
+++ assignment 3 of mycaptain.py
@@ -0,0 +1,17 @@
+#1)write a python program to print all positive numbers in the range.
+list1=[12,-7,5,64,-14]
+for i in range(len(list1)):
+    if list1[i]>=0:
+        a=list1[i]
+        print(a,end=",")
+
+print("\n")
+
+
+#2)
+list2=[12,14,-95,3]
+for j in range(len(list2)):
+    if list2[j]>=0:
+        b=list2[j]
+        print(b,end=",")
+
